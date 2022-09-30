3de183c289d173b44a6160e25d29fd12712f8ec9
Deftool66 <deftool@gmx.net>
2022-09-30 13:27:25 +0200

    added new codewars challenge

---
+++ 6kyu/Convert string to camel case.rb
@@ -0,0 +1,17 @@
+# DESCRIPTION:
+# Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized (known as Upper Camel Case, also often referred to as Pascal case).
+
+# Examples
+# "the-stealth-warrior" gets converted to "theStealthWarrior"
+# "The_Stealth_Warrior" gets converted to "TheStealthWarrior"
+
+#mysolution
+
+def to_camel_case(str)
+  #a function that convert a dash/underscore delimited words into an Upper Camel Case string.
+  array  = str.split(/_|-/)
+  first_word = array.first
+  array.shift
+  "#{first_word}" + "#{array.map{|word| word.capitalize}.join("")}"
+
+end

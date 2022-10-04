262714b28bd2d289bcda2538684daf36297c022b
Deftool66 <deftool@gmx.net>
2022-10-04 12:47:18 +0200

    are they the same? solved

--- 6kyu/Are_they_the_same?.rb
+++ 6kyu/Are_they_the_same?.rb
@@ -1,3 +1,3 @@
-def comp(array1, array2)
-  array1.nil? || array2.nil? ? false : array1.map!{ |num| num * num }.sort == array2.sort
+def are_the_the_same?(array1, array2)
+  array1.nil? || array2.nil? ? false : array1.map! { |num| num * num }.sort == array2.sort
 end
